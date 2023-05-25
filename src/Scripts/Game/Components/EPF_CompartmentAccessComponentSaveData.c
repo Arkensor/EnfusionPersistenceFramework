f4ca18d1d5b88b8a466bdd2a5d10edd52e85261c
Arkensor <arkensor@gmail.com>
2023-05-25 19:17:19 +0200

    Add max range for char compartment save data to spawn back into the vehicle

--- src/Scripts/Game/Components/EPF_CompartmentAccessComponentSaveData.c
+++ src/Scripts/Game/Components/EPF_CompartmentAccessComponentSaveData.c
@@ -1,6 +1,8 @@
 [EPF_ComponentSaveDataType(CompartmentAccessComponent), BaseContainerProps()]
 class EPF_CompartmentAccessComponentSaveDataClass : EPF_ComponentSaveDataClass
 {
+	[Attribute(defvalue: "50", desc: "Maximum range the vehicle can have moved for the character to spawn back in.\nIf further away character will spawn where he was last in the world.\n-1 To disable the check.")]
+	int m_iMaxPassangerLoadRange;
 };
 
 [EDF_DbName.Automatic()]
@@ -57,10 +59,17 @@ class EPF_CompartmentAccessComponentSaveData : EPF_ComponentSaveData
 	//------------------------------------------------------------------------------------------------
 	override EPF_EApplyResult ApplyTo(IEntity owner, GenericComponent component, EPF_ComponentSaveDataClass attributes)
 	{
+		EPF_CompartmentAccessComponentSaveDataClass settings = EPF_CompartmentAccessComponentSaveDataClass.Cast(attributes);
 		CompartmentAccessComponent compartment = CompartmentAccessComponent.Cast(component);
+
+		vector currentPos = owner.GetOrigin();
+
+		// compartment holder and compartment entity can not be too far apart.
 		IEntity compartmentHolder = EPF_PersistenceManager.GetInstance().FindEntityByPersistentId(m_sEntity);
 		BaseCompartmentManagerComponent compartmentManager = EPF_Component<BaseCompartmentManagerComponent>.Find(compartmentHolder);
-		if (compartmentManager)
+		if (compartmentManager && 
+			(settings.m_iMaxPassangerLoadRange == -1 || 
+			vector.Distance(compartmentHolder.GetOrigin(), currentPos) <= settings.m_iMaxPassangerLoadRange))
 		{
 			array<BaseCompartmentSlot> outCompartments();
 			compartmentManager.GetCompartments(outCompartments);
@@ -72,7 +81,6 @@ class EPF_CompartmentAccessComponentSaveData : EPF_ComponentSaveData
 		}
 
 		// No longer able to enter compartment so find free position on the ground to spawn instead
-		vector currentPos = owner.GetOrigin();
 		if (SCR_WorldTools.FindEmptyTerrainPosition(currentPos, currentPos, 50, cylinderHeight: 1000))
 			EPF_Utils.Teleport(owner, currentPos);
 
