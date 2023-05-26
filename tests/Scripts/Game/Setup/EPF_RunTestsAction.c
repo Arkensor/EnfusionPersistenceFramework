914011e422e9d76baee4769377935436295698cd
Arkensor <arkensor@gmail.com>
2023-05-26 08:44:46 +0200

    I was stupid - again

--- tests/Scripts/Game/Setup/EPF_RunTestsAction.c
+++ tests/Scripts/Game/Setup/EPF_RunTestsAction.c
@@ -1,23 +1,3 @@
-sealed class TAG_MyComponentClass : ScriptComponentClass
-{
-	//------------------------------------------------------------------------------------------------
-	static override bool DependsOn(string className)
-	{
-		DoSomething(className.ToType(), {Class});
-		return false;
-	}
-
-	//------------------------------------------------------------------------------------------------
-	static void DoSomething(typename type, notnull array<typename> from)
-	{
-		Print(type);
-	}
-};
-
-class TAG_MyComponent : ScriptComponent
-{
-};
-
 class EPF_RunTestsAction : ScriptedUserAction
 {
 	//------------------------------------------------------------------------------------------------
