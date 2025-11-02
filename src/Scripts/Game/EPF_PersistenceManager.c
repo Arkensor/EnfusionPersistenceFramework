76350aa13c6dcc8f034b2e199173f80afcbf8334
Arkensor <arkensor@gmail.com>
2025-11-02 18:57:56 +0100

    Fixed: Peertool clients would be treated as baked entities

--- src/Scripts/Game/EPF_PersistenceManager.c
+++ src/Scripts/Game/EPF_PersistenceManager.c
@@ -477,7 +477,8 @@ class EPF_PersistenceManager
 
 		// "Hacky" restore of baked status.
 		// If id no longer provides this info, record all ids created up until world init and remember those as baked to check here.
-		if (id.StartsWith("00bb"))
+		// Ignore peertool polyfill ids
+		if (id.StartsWith("00bb") && !id.StartsWith("00bbbddd-"))
 			persistenceComponent.FlagAsBaked();
 
 		EPF_PersistenceComponentClass settings = EPF_ComponentData<EPF_PersistenceComponentClass>.Get(persistenceComponent);
